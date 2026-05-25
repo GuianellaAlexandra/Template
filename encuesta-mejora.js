703caec0caf32a34d99fb9c6ac6f7609c1e0fac3
GuianellaAlexandra <guianellaalexandra.10@gmail.com>
2026-05-24 22:08:44 -0500

    Update encuesta-mejora.js

--- encuesta-mejora.js
+++ encuesta-mejora.js
@@ -15,7 +15,7 @@
   const next = document.getElementById('jpNext');
   const tabs = canvas.querySelectorAll('.jp-tab');
 
-  const apiEndpoint = 'https://c9b00e1ba24ee8aaa60b8a38b3aa92.e9.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/24f4df3d68d5436bb2993c64c2d30124/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=w_J9yOhsqqK0PGn0wt_MPrFUKQArWtsOYCpgQjUpyjs';
+  const apiEndpoint = canvas.dataset.apiEndpoint;
   const antiforgeryToken = canvas.dataset.antiforgeryToken;
   const userName = canvas.dataset.userName;
   const userEmail = canvas.dataset.userEmail;
